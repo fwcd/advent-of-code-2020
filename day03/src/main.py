e1006653b7d6cae575be6d733b426597f5c8a53f
fwcd <fwcdmail@gmail.com>
2020-12-03 06:22:30 +0100

    Solve day 3 part 2

--- day03/src/main.py
+++ day03/src/main.py
@@ -1,3 +1,5 @@
+import functools
+
 def count_trees(forest, right, down):
     i = 1
     count = 0
@@ -10,8 +12,12 @@ def count_trees(forest, right, down):
 def main():
     with open("resources/input.txt", "r") as f:
         forest = [l.strip() for l in f.readlines()]
+
         part1 = count_trees(forest, 3, 1)
         print(f"Part 1: {part1}")
+
+        part2 = functools.reduce(lambda x, y: x * y, [count_trees(forest, r, d) for (r, d) in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]])
+        print(f"Part 2: {part2}")
 
 if __name__ == "__main__":
     main()
