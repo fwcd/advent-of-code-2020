74593ce33bb171381f17e07eb92753f4523a589c
fwcd <fwcdmail@gmail.com>
2020-12-03 06:13:39 +0100

    Begin with day 3

---
+++ day03/src/main.py
@@ -0,0 +1,17 @@
+def count_trees(forest, right, down):
+    i = 0
+    count = 0
+    while (i * down) < len(forest):
+        if forest[i * down][(i * right) % len(forest[0])] == "#":
+            count += 1
+        i += 1
+    return count
+
+def main():
+    with open("resources/input.txt", "r") as f:
+        forest = f.readlines()
+        part1 = count_trees(forest, 3, 1)
+        print(f"Part 1: {part1}")
+
+if __name__ == "__main__":
+    main()
