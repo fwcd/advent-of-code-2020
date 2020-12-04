cbfda273b7f370bd13495c294ac8ee57a4718a06
fwcd <fwcdmail@gmail.com>
2020-12-04 06:00:33 +0100

    Read lines from skeletal input

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,3 +1,7 @@
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+
 fn main() {
+    let lines = BufReader::new(File::open("resources/input.txt").unwrap()).lines();
     println!("Hello, world!");
 }
