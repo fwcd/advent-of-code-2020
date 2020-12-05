46abc86fc638f9a02e034bb074fdb0cec62e3b75
fwcd <fwcdmail@gmail.com>
2020-12-05 06:39:31 +0100

    Implement day 5 part 1

--- day05/day05.ml
+++ day05/day05.ml
@@ -1,6 +1,13 @@
-let replace s x y = Seq.map (fun v -> if v == x then y else v) s
+let replace s x y = Seq.map (fun v -> if v == x then y else v) s;;
 
-let binary_section s l r = int_of_string (String.of_seq (List.to_seq ('0' :: 'b' :: List.of_seq (replace l r (String.to_seq s)))))
+let binary_section s l r =
+    let bin_literal = (String.of_seq (List.to_seq ('0' :: 'b' :: List.of_seq (replace (replace (String.to_seq s) l '0') r '1')))) in
+    int_of_string bin_literal;;
+
+let seat_id raw =
+    let row = binary_section (String.sub raw 0 7) 'F' 'B' in
+    let col = binary_section (String.sub raw 7 3) 'L' 'R' in
+    row * 8 + col;;
 
 let read_lines filename =
     (* https://stackoverflow.com/questions/5774934/how-do-i-read-in-lines-from-a-text-file-in-ocaml/5775024#5775024 *)
@@ -15,5 +22,7 @@ let read_lines filename =
     List.rev !lines;;
 
 let () =
-    List.iter print_endline (read_lines "resources/input.txt")
+    let seat_ids = List.map seat_id (read_lines "resources/input.txt") in
+    let part1 = List.fold_left max 0 seat_ids in
+    Printf.printf "Part 1: %d" part1;;
     
