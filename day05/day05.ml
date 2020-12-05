546bace1bd844c606f68f34d067b54ed66363641
fwcd <fwcdmail@gmail.com>
2020-12-05 06:28:53 +0100

    Implement binary_section and replace for day 5

--- day05/day05.ml
+++ day05/day05.ml
@@ -1,3 +1,7 @@
+let replace s x y = Seq.map (fun v -> if v == x then y else v) s
+
+let binary_section s l r = int_of_string (String.of_seq (List.to_seq ('0' :: 'b' :: List.of_seq (replace l r (String.to_seq s)))))
+
 let read_lines filename =
     (* https://stackoverflow.com/questions/5774934/how-do-i-read-in-lines-from-a-text-file-in-ocaml/5775024#5775024 *)
     let lines = ref [] in
@@ -8,7 +12,7 @@ let read_lines filename =
         done; !lines
     with End_of_file ->
         close_in channel;
-    List.rev !lines
+    List.rev !lines;;
 
 let () =
     List.iter print_endline (read_lines "resources/input.txt")
