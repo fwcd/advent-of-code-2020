222146c596790a87a90c8dfa186ad2d3df7e48e1
fwcd <fwcdmail@gmail.com>
2020-12-05 21:27:55 +0100

    Read sample input in day 6 stub

--- day06/Program.fs
+++ day06/Program.fs
@@ -1,13 +1,9 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
-open System
-
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+open System.IO
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    let lns = File.ReadAllLines("resources/input.txt")
+    printfn "%s" (String.concat " " lns)
     0 // return an integer exit code
