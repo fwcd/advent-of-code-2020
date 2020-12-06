15a27f7fb698300d7465fc5d89c484d3ddf962c0
fwcd <fwcdmail@gmail.com>
2020-12-06 06:21:23 +0100

    Solve day 6 part 1 in F#

--- day06/Program.fs
+++ day06/Program.fs
@@ -4,6 +4,8 @@ open System.IO
 
 [<EntryPoint>]
 let main argv =
-    let lns = File.ReadAllLines("resources/input.txt")
-    printfn "%s" (String.concat " " lns)
+    let lns = File.ReadAllText("resources/input.txt")
+    let groups = lns.Split("\n\n")
+    let customs = groups |> Seq.map ((fun g -> g.Replace("\n", "")) >> Seq.distinct >> Seq.length)
+    printfn "Part 1: %d" (Seq.sum customs)
     0 // return an integer exit code
