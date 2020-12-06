b36747bf2c39ccbd134c16d5df416b2cc329b410
fwcd <fwcdmail@gmail.com>
2020-12-06 06:46:32 +0100

    Solve day 6 part 2 in F#

--- day06/Program.fs
+++ day06/Program.fs
@@ -2,10 +2,14 @@
 
 open System.IO
 
+let fold1 f zs = match Seq.toList zs with
+                 | x :: xs -> Seq.fold f x xs
+                 | [] -> raise (System.ArgumentException "fold1 needs non-empty list!")
+
 [<EntryPoint>]
 let main argv =
     let lns = File.ReadAllText("resources/input.txt")
-    let groups = lns.Split("\n\n")
-    let customs = groups |> Seq.map ((fun g -> g.Replace("\n", "")) >> Seq.distinct >> Seq.length)
-    printfn "Part 1: %d" (Seq.sum customs)
+    let groups = lns.Split("\n\n") |> Seq.map ((fun g -> g.Split("\n") |> Seq.map Set.ofSeq |> Seq.filter (Seq.isEmpty >> not)))
+    let part2 = groups |> Seq.map (fold1 Set.intersect >> Seq.length)
+    printfn "Part 2: %d" (Seq.sum part2)
     0 // return an integer exit code
