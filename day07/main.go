4878dcb31813c4ed67dae0b01e73ceff35f76cb4
fwcd <fwcdmail@gmail.com>
2020-12-07 07:12:49 +0100

    Create bag graph for day 7

--- day07/main.go
+++ day07/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"regexp"
 )
 
 func check(e error) {
@@ -17,4 +18,27 @@ func main() {
 
 	input := string(data)
 	fmt.Println(input)
+
+	rulePattern, err := regexp.Compile("([a-z ]+) bags contain ([^\\.]+)\\.")
+	check(err)
+
+	containedPattern, err := regexp.Compile("(\\d+) ([a-z ]+) bag")
+	check(err)
+
+	graph := make(map[string][]string)
+	rules := rulePattern.FindAllStringSubmatch(input, -1)
+
+	for _, rule := range rules {
+		outerBag := rule[1]
+		for _, contained := range containedPattern.FindAllStringSubmatch(rule[2], -1) {
+			innerBag := contained[1]
+			_, exists := graph[innerBag]
+			if !exists {
+				graph[innerBag] = make([]string, 0)
+			}
+			graph[innerBag] = append(graph[innerBag], outerBag)
+		}
+	}
+
+	fmt.Print(graph)
 }
