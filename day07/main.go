2e3e2dcbec17b9b239f8c5e9c24799c405eaac33
fwcd <fwcdmail@gmail.com>
2020-12-07 07:33:25 +0100

    Implement part 1 of day 7 in Go

--- day07/main.go
+++ day07/main.go
@@ -12,12 +12,22 @@ func check(e error) {
 	}
 }
 
+func dfs(inner string, graph map[string][]string, visited *map[string]bool) {
+	for _, outer := range graph[inner] {
+		_, ok := (*visited)[outer]
+		fmt.Println("Outer", outer, ok)
+		if !ok {
+			(*visited)[outer] = true
+			dfs(outer, graph, visited)
+		}
+	}
+}
+
 func main() {
 	data, err := ioutil.ReadFile("resources/input.txt")
 	check(err)
 
 	input := string(data)
-	fmt.Println(input)
 
 	rulePattern, err := regexp.Compile("([a-z ]+) bags contain ([^\\.]+)\\.")
 	check(err)
@@ -31,14 +41,17 @@ func main() {
 	for _, rule := range rules {
 		outerBag := rule[1]
 		for _, contained := range containedPattern.FindAllStringSubmatch(rule[2], -1) {
-			innerBag := contained[1]
-			_, exists := graph[innerBag]
-			if !exists {
+			innerBag := contained[2]
+			_, ok := graph[innerBag]
+			if !ok {
 				graph[innerBag] = make([]string, 0)
 			}
 			graph[innerBag] = append(graph[innerBag], outerBag)
 		}
 	}
 
-	fmt.Print(graph)
+	fmt.Println(graph)
+	reachable := make(map[string]bool)
+	dfs("shiny gold", graph, &reachable)
+	fmt.Println("Part 1:", len(reachable))
 }
