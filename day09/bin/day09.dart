f308bc4b167604b728a967189b58dece4c656620
fwcd <fwcdmail@gmail.com>
2020-12-08 18:28:58 +0100

    Set up dart project for day 9

---
+++ day09/bin/day09.dart
@@ -0,0 +1,5 @@
+import 'package:day09/day09.dart' as day09;
+
+void main(List<String> arguments) {
+  print('Hello world: ${day09.calculate()}!');
+}
---
+++ day09/test/day09_test.dart
@@ -0,0 +1,8 @@
+import 'package:day09/day09.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
