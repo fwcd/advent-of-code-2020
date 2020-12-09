d664898dc9379ad66ca1f6a4a7bb0496ee098aaf
fwcd <fwcdmail@gmail.com>
2020-12-09 07:10:40 +0100

    Add part 2 of day 9

--- day09/bin/day09.dart
+++ day09/bin/day09.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'dart:math';
+
 bool isXMAS(List<int> xs, int i, int n) {
   for (final x in xs.getRange(i - n, i)) {
     for (final y in xs.getRange(i - n, i)) {
@@ -11,6 +13,16 @@ bool isXMAS(List<int> xs, int i, int n) {
   return false;
 }
 
+List<int> contiguousSum(List<int> xs, int i, int expected) {
+  var j = i + 1;
+  var sum = xs[i];
+  while (j < xs.length && sum < expected) {
+    sum += xs[j];
+    j++;
+  }
+  return sum == expected ? xs.getRange(i, j).toList() : null;
+}
+
 Future<void> main(List<String> arguments) async {
   final input = await File('resources/input.txt').readAsString();
   final xs = input.split('\n').map((l) => int.tryParse(l)).where((x) => x != null).toList();
@@ -19,4 +31,10 @@ Future<void> main(List<String> arguments) async {
     .firstWhere((i) => !isXMAS(xs, i, n));
   final part1 = xs[part1Index];
   print('Part 1: $part1 at $part1Index');
+
+  final part2Cont = List<int>.generate(xs.length, (i) => i)
+    .map((i) => contiguousSum(xs, i, part1))
+    .firstWhere((x) => x != null);
+  final part2 = part2Cont.reduce(min) + part2Cont.reduce(max);
+  print('Part 2: $part2 from $part2Cont');
 }
