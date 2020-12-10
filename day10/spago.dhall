4bae5422a2e51c34bd3b72c9fe7ad81b05086634
fwcd <fwcdmail@gmail.com>
2020-12-10 06:36:24 +0100

    Parse day 10 input

--- day10/spago.dhall
+++ day10/spago.dhall
@@ -4,7 +4,7 @@ You can edit this file as you like.
 -}
 { name = "my-project"
 , dependencies =
-  [ "console", "effect", "node-buffer", "node-fs", "psci-support" ]
+  [ "console", "effect", "node-buffer", "node-fs", "parseint", "psci-support" ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- day10/src/Main.purs
+++ day10/src/Main.purs
@@ -2,13 +2,16 @@ module Main where
 
 import Prelude
 
+import Data.Array (catMaybes)
+import Data.Int.Parse (parseInt, toRadix)
+import Data.String (Pattern(..), split)
 import Effect (Effect)
 import Effect.Console (log)
-
-import Node.Encoding (Encoding (..))
+import Node.Encoding (Encoding(..))
 import Node.FS.Sync (readTextFile)
 
 main :: Effect Unit
 main = do
   input <- readTextFile UTF8 "resources/input.txt"
-  log input
+  let joltages = catMaybes $ map (flip parseInt $ toRadix 10) $ split (Pattern "\n") input
+  log $ show joltages
