5a8672d923373474f029c3f9c7b783888edd49c5
fwcd <fwcdmail@gmail.com>
2020-12-10 07:36:28 +0100

    WIP: Try implementing part 2 of day 10

--- day10/src/Main.purs
+++ day10/src/Main.purs
@@ -2,11 +2,13 @@ module Main where
 
 import Prelude
 
-import Data.Array (catMaybes, filter, head, length, sort, tail, take, (:))
-import Data.Foldable (maximum)
+import Data.Array (catMaybes, deleteAt, drop, elem, filter, group, head, length, modifyAt, nub, sort, tail, take, (!!), (..), (:))
+import Data.Array.NonEmpty (toArray)
+import Data.Foldable (maximum, product, sum)
 import Data.Int.Parse (parseInt, toRadix)
 import Data.Maybe (Maybe(..))
 import Data.String (Pattern(..), split)
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Console (log)
 import Node.Encoding (Encoding(..))
@@ -22,17 +24,42 @@ joltagePath j maxJ as | j == maxJ - 3 = Just [3]
                           Just ds -> Just $ (a - j) : ds
                           Nothing -> joltagePath j maxJ as'
 
+maybeToArray :: forall a. Maybe a -> Array a
+maybeToArray (Just x) = [x]
+maybeToArray Nothing  = []
+
+countArrangements :: Array Int -> Int
+countArrangements [x] | x <= 3 = 1
+countArrangements xs = sum $ do
+  i <- 0 .. (length xs - 1)
+  j <- (i + 1) .. (length xs - 1)
+  x <- maybeToArray $ xs !! i
+  y <- maybeToArray $ xs !! j
+
+  if x + y == 3 then 
+    [1]
+  else if x + y > 3 then
+    []
+  else do
+    xs'  <- maybeToArray $ deleteAt j xs
+    xs'' <- maybeToArray $ modifyAt i (add j) xs'
+    pure $ 1 + countArrangements xs''
+
 main :: Effect Unit
 main = do
   input <- readTextFile UTF8 "resources/input.txt"
   let joltages = sort $ catMaybes $ map (flip parseInt $ toRadix 10) $ split (Pattern "\n") input
-      part1Maybe = do
+      parts = do
         m <- maximum joltages
         path <- joltagePath 0 (m + 3) joltages
-        pure $ length (filter (eq 1) path) * length (filter (eq 3) path)
+        let part1 = length (filter (eq 1) path) * length (filter (eq 3) path)
+            chunksOf1s = map toArray $ filter (elem 1) $ group path
+            part2 = product $ map countArrangements chunksOf1s
+        pure $ Tuple part1 part2
 
-  case part1Maybe of
-    Just part1 -> do
-      log $ show part1
+  case parts of
+    Just (Tuple part1 part2) -> do
+      log $ "Part 1: " <> show part1
+      log $ "Part 2: " <> show part2
     Nothing -> do
       log "No joltages!"
