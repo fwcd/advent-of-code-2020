f8631cc7a8a105cf0872c4ad51fc8e2c263ff5bc
fwcd <fwcdmail@gmail.com>
2020-12-11 09:43:18 +0100

    Add part 2 template for day 11

--- day11/source/app.d
+++ day11/source/app.d
@@ -15,7 +15,10 @@ private dchar[] neighbors(dchar[][] grid, int x, int y) {
 	return nbs.array;
 }
 
-private dchar[][] step(dchar[][] input)
+// Higher-order functions are slow, therefore we have some redundancy
+// among the step/simulation functions between part 1 and 2.
+
+private dchar[][] part1Step(dchar[][] input)
 {
 	dchar[][] result = input.map!(xs => xs.array).array;
 	bool changed = false;
@@ -40,6 +43,60 @@ private dchar[][] step(dchar[][] input)
 	return changed ? result : null;
 }
 
+private dchar[][] part2Step(dchar[][] input)
+{
+	dchar[][] result = input.map!(xs => xs.array).array;
+	bool changed = false;
+
+	for (int x = 0; x < input.length; x++) {
+		for (int y = 0; y < input[0].length; y++) {
+			const nbs = neighbors(input, x, y);
+			const occupiedNbs = nbs.filter!(c => c == OCCUPIED).array;
+
+			if (input[x][y] == EMPTY && occupiedNbs.length == 0) {
+				// Get occupied
+				result[x][y] = OCCUPIED;
+			} else if (input[x][y] == OCCUPIED && occupiedNbs.length >= 4) {
+				// Get empty
+				result[x][y] = EMPTY;
+			}
+
+			changed |= input[x][y] != result[x][y];
+		}
+	}
+
+	return changed ? result : null;
+}
+
+private dchar[][] simulatePart1(dchar[][] grid)
+{
+	while (true) {
+		dchar[][] next = part1Step(grid);
+		if (next != null) {
+			grid = next;
+		} else {
+			return grid;
+		}
+	}
+}
+
+private dchar[][] simulatePart2(dchar[][] grid)
+{
+	while (true) {
+		dchar[][] next = part2Step(grid);
+		if (next != null) {
+			grid = next;
+		} else {
+			return grid;
+		}
+	}
+}
+
+private ulong totalOccupied(dchar[][] grid)
+{
+	return grid.map!(l => l).joiner.filter!(c => c == OCCUPIED).array.length;
+}
+
 void main()
 {
 	dchar[][] grid = readText("resources/input.txt")
@@ -48,19 +105,6 @@ void main()
 		.map!(line => to!(dchar[])(line.toUTF32))
 		.array;
 
-	bool running = true;
-	int i = 0;
-	while (running) {
-		// writefln("%s", grid.map!(cs => cs.toUTF8).join());
-		dchar[][] next = step(grid);
-		if (next != null) {
-			grid = next;
-		} else {
-			running = false;
-		}
-		i++;
-	}
-
-	auto part1 = grid.map!(l => l).joiner.filter!(c => c == OCCUPIED).array.length;
-	writefln("Part 1: %d", part1);
+	writefln("Part 1: %d", totalOccupied(simulatePart1(grid)));
+	writefln("Part 2: %d", totalOccupied(simulatePart2(grid)));
 }
