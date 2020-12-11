69dc0368e451bdf8d2c2bbb87cc556acd28b5fee
fwcd <fwcdmail@gmail.com>
2020-12-11 16:03:49 +0100

    Read input text in day 12 sample

--- day12/src/day12.pas
+++ day12/src/day12.pas
@@ -1,5 +1,24 @@
 program Day12;
 
+{$mode objFPC}
+
+uses
+    Sysutils;
+
+var
+    fileIn: TextFile;
+    input: String;
+
 begin
-    writeln('Hello AoC!');
+    AssignFile(fileIn, 'resources/input.txt');
+    try
+        reset(fileIn);
+        while not eof(fileIn) do
+        begin
+            readln(fileIn, input);
+            writeln(input);
+        end;
+    finally
+        CloseFile(fileIn);
+    end;
 end.
