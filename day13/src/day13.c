9a425e80dbe432079d2972785c440db6a774c0c7
fwcd <fwcdmail@gmail.com>
2020-12-13 14:41:26 +0100

    Read file in day 13 and update gitignore

--- day13/src/day13.c
+++ day13/src/day13.c
@@ -3,9 +3,29 @@
 
 int main(void) {
     FILE *file = fopen("resources/input.txt", "r");
-    char rawNum[1024] = {0};
-    while (fscanf(file, "%s\n", rawNum) > 0) {
-        printf("Line: %s\n", rawNum);
+    int startTs;
+    int busses[1024] = {0};
+    int i = 0;
+    fscanf(file, "%d\n", &startTs);
+
+    char rawBus[16];
+    while (fscanf(file, "%[^,],", rawBus) > 0) {
+        char *end = rawBus;
+        int bus = (int) strtol(rawBus, &end, 10);
+        if (rawBus != end) {
+            busses[i++] = bus;
+        } else {
+            busses[i++] = -1;
+        }
     }
+
+    busses[i] = 0;
     fclose(file);
+    printf("Read %d numbers: ", i);
+    int *bus = busses;
+    while (*bus) {
+        printf("%d, ", *bus);
+        bus++;
+    }
+    return 0;
 }
