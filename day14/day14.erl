3b80fb0e7581337ab2e5e58f7ae99efc7dc86131
fwcd <fwcdmail@gmail.com>
2020-12-14 21:39:46 +0100

    Fix even more parsing issues in day 14

--- day14/day14.erl
+++ day14/day14.erl
@@ -1,37 +1,42 @@
 -module(day14).
 -export([main/1]).
+-mode(compile).
 
 -record(machine, {mask=error, tape=dict:new()}).
 
 parse_instruction(Raw) ->
     Split = lists:map(fun string:trim/1, string:split(Raw, "=")),
     case Split of
-        ["mask", Rhs]                     -> {mask, Rhs};
-        [[$m, $e, $m, $[, RawN, $]], Rhs] -> {mem, string:to_integer(RawN), string:to_integer(Rhs)}
+        ["mask", Rhs]          -> {mask, Rhs};
+        ["mem" ++ Access, Rhs] -> 
+            {N, _} = string:to_integer(string:slice(Access, 1, string:length(Access) - 2)),
+            {X, _} = string:to_integer(Rhs),
+            {mem, N, X}
     end.
 
 parse_program(Raw) ->
     lists:map(fun(RawInst) -> parse_instruction(RawInst) end, string:tokens(Raw, "\n")).
 
 apply_mask(Mask, X) ->
-    OrMask = string:to_integer(string:replace(Mask, "X", "0")),
-    AndMask = string:to_integer(string:replace(Mask, "X", "1")),
+    {OrMask, _} = string:to_integer(string:replace(Mask, "X", "0")),
+    {AndMask, _} = string:to_integer(string:replace(Mask, "X", "1")),
     (X band AndMask) bor OrMask.
 
 run_instruction(Inst, Machine) ->
     case Inst of
         {mask, Rhs}   -> Machine#machine{mask=Rhs};
-        {mem, N, Rhs} ->
+        {mem, N, X} ->
             #machine{mask=Mask, tape=Tape} = Machine,
-            Tape2 = dict:update(N, fun(_) -> apply_mask(Mask, Rhs) end, Tape),
+            Y = apply_mask(Mask, X),
+            io:fwrite("At ~B writing ~B~n", [N, X]),
+            Tape2 = dict:store(N, Y, Tape),
             Machine#machine{tape=Tape2}
     end.
 
 run_program([], Machine) -> Machine;
-run_program([RawInst|RawInsts], Machine) ->
-    Inst = parse_instruction(RawInst),
+run_program([Inst|Insts], Machine) ->
     Machine2 = run_instruction(Inst, Machine),
-    run_program(RawInsts, Machine2).
+    run_program(Insts, Machine2).
 
 main(_) ->
     {ok, BinInput} = file:read_file("resources/input.txt"),
@@ -40,4 +45,4 @@ main(_) ->
     Machine = #machine{},
     Machine2 = run_program(Prog, Machine),
     Part1 = lists:sum(lists:map(fun ({_, X}) -> X end, dict:to_list(Machine2#machine.tape))),
-    io:fwrite("Part 1: ~B", [Part1]).
+    io:fwrite("Part 1: ~B~n", [Part1]).
