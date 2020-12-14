721dcc26d0f2e36ec7175fb68f5cf335f46fbb8a
fwcd <fwcdmail@gmail.com>
2020-12-14 22:44:05 +0100

    WIP: Generate part 2 floating addresses in day 14

--- day14/day14.erl
+++ day14/day14.erl
@@ -1,5 +1,5 @@
 -module(day14).
--export([main/1]).
+-export([main/1, floating_addresses/1]).
 -mode(compile).
 
 -record(machine, {mask=error, tape=dict:new()}).
@@ -7,6 +7,8 @@
 replace([], _, _) -> [];
 replace([E|Es], E, G) -> [G|replace(Es, E, G)];
 replace([E|Es], F, G) -> [E|replace(Es, F, G)].
+
+enumerate(Es) -> lists:zip(lists:seq(0, length(Es) - 1), Es).
 
 parse_instruction(Raw) ->
     Split = lists:map(fun string:trim/1, string:split(Raw, "=")),
@@ -21,12 +23,8 @@ parse_instruction(Raw) ->
 parse_program(Raw) ->
     lists:map(fun(RawInst) -> parse_instruction(RawInst) end, string:tokens(Raw, "\n")).
 
-apply_mask(Mask, X) ->
-    OrMask = list_to_integer(replace(Mask, $X, $0), 2),
-    AndMask = list_to_integer(replace(Mask, $X, $1), 2),
-    (X band AndMask) bor OrMask.
-
 set_memory(N, X, Machine) ->
+    io:fwrite("memory[~B] <- ~B~n", [N, X]),
     Tape = Machine#machine.tape,
     Tape2 = dict:store(N, X, Tape),
     Machine#machine{tape=Tape2}.
@@ -46,18 +44,51 @@ run_program([Inst|Insts], Machine, AccessHandler) ->
     run_program(Insts, Machine2, AccessHandler).
 
 sum_memory(Machine) ->
-    lists:sum(lists:map(fun ({_, X}) -> X end, dict:to_list(Machine#machine.tape))).
+    lists:sum(lists:map(fun({_, X}) -> X end, dict:to_list(Machine#machine.tape))).
+
+apply_part1_mask(Mask, X) ->
+    OrMask = list_to_integer(replace(Mask, $X, $0), 2),
+    AndMask = list_to_integer(replace(Mask, $X, $1), 2),
+    (X band AndMask) bor OrMask.
+
+floating_raw_addresses(Acc, []) -> [lists:reverse(Acc)];
+floating_raw_addresses(Acc, [B|Bs]) ->
+    case B of
+        $X -> floating_raw_addresses(Acc, [$0|Bs]) ++ floating_raw_addresses(Acc, [$1|Bs]);
+        _ -> floating_raw_addresses([B|Acc], Bs)
+    end.
+
+floating_addresses(Mask) ->
+    lists:map(fun list_to_integer/1, floating_raw_addresses("", Mask)).
+
+decode_part2_mask(Mask, N) ->
+    BitCount = length(Mask),
+    lists:map(fun({I, B}) ->
+        case B of
+            $0 ->
+                Bit = (N bsl (BitCount - 1 - I)) band 1,
+                [B2|_] = integer_to_list(Bit),
+                B2;
+            _ -> B
+        end
+    end, enumerate(Mask)).
 
 part1_handler(Mask, N, X) ->
-    [{N, apply_mask(Mask, X)}].
+    [{N, apply_part1_mask(Mask, X)}].
+
+part2_handler(Mask, N, X) ->
+    Mask2 = decode_part2_mask(Mask, N),
+    % io:fwrite("Addrs: [~s]~n", [lists:join(", ", lists:map(fun erlang:integer_to_list/1, floating_addresses(Mask2)))]),
+    lists:map(fun(M) -> {M, X} end, floating_addresses(Mask2)).
 
 main(_) ->
-    {ok, BinInput} = file:read_file("resources/input.txt"),
+    {ok, BinInput} = file:read_file("resources/example2.txt"),
     Input = unicode:characters_to_list(BinInput),
     Prog = parse_program(Input),
     Machine = #machine{},
 
     Part1 = sum_memory(run_program(Prog, Machine, fun part1_handler/3)),
-    io:fwrite("Part 1: ~B~n", [Part1]).
-    % Part2 = sum_memory(run_program(Prog, Machine, fun part2_handler/3)),
-    % io:fwrite("Part 2: ~B~n", [Part2]).
+    io:fwrite("Part 1: ~B~n", [Part1]),
+
+    Part2 = sum_memory(run_program(Prog, Machine, fun part2_handler/3)),
+    io:fwrite("Part 2: ~B~n", [Part2]).
