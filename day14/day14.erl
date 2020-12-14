7b275a6ffaebe2e58c909df3aa0d1488b9454a9f
fwcd <fwcdmail@gmail.com>
2020-12-14 21:26:09 +0100

    WIP: Fix some parsing issues in day 14

--- day14/day14.erl
+++ day14/day14.erl
@@ -1,16 +1,17 @@
 -module(day14).
 -export([main/1]).
 
--record(machine, {mask=error, tape=#{}}).
+-record(machine, {mask=error, tape=dict:new()}).
 
 parse_instruction(Raw) ->
-    case lists:map(fun string:trim/1, string:split(Raw, "=")) of
-        ["mask", Rhs]                  -> {mask, Rhs};
-        ["mem" ++ [$[, RawN, $]], Rhs] -> {mem, string:to_integer(RawN), Rhs}
+    Split = lists:map(fun string:trim/1, string:split(Raw, "=")),
+    case Split of
+        ["mask", Rhs]                     -> {mask, Rhs};
+        [[$m, $e, $m, $[, RawN, $]], Rhs] -> {mem, string:to_integer(RawN), string:to_integer(Rhs)}
     end.
 
 parse_program(Raw) ->
-    string:split(Raw, "\n").
+    lists:map(fun(RawInst) -> parse_instruction(RawInst) end, string:tokens(Raw, "\n")).
 
 apply_mask(Mask, X) ->
     OrMask = string:to_integer(string:replace(Mask, "X", "0")),
@@ -18,11 +19,12 @@ apply_mask(Mask, X) ->
     (X band AndMask) bor OrMask.
 
 run_instruction(Inst, Machine) ->
-    case parse_instruction(inst) of
+    case Inst of
         {mask, Rhs}   -> Machine#machine{mask=Rhs};
         {mem, N, Rhs} ->
-            Tape = Machine#machine.tape,
-            Machine#machine{tape=Tape}
+            #machine{mask=Mask, tape=Tape} = Machine,
+            Tape2 = dict:update(N, fun(_) -> apply_mask(Mask, Rhs) end, Tape),
+            Machine#machine{tape=Tape2}
     end.
 
 run_program([], Machine) -> Machine;
@@ -32,9 +34,10 @@ run_program([RawInst|RawInsts], Machine) ->
     run_program(RawInsts, Machine2).
 
 main(_) ->
-    {ok, Input} = file:read_file("resources/input.txt"),
+    {ok, BinInput} = file:read_file("resources/input.txt"),
+    Input = unicode:characters_to_list(BinInput),
     Prog = parse_program(Input),
     Machine = #machine{},
     Machine2 = run_program(Prog, Machine),
-    Part1 = lists:sum(lists:map(fun ({_, X}) -> X end, Machine2#machine.tape)),
+    Part1 = lists:sum(lists:map(fun ({_, X}) -> X end, dict:to_list(Machine2#machine.tape))),
     io:fwrite("Part 1: ~B", [Part1]).
