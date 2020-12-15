f5571837fa557bc98286241bde5d69797ecfe230
fwcd <fwcdmail@gmail.com>
2020-12-15 09:55:07 +0100

    Read file in day 15 (VB.NET)

--- day15/Program.vb
+++ day15/Program.vb
@@ -1,7 +1,9 @@
 Imports System
+Imports System.IO
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Dim input As String() = File.ReadAllLines("resources/input.txt")
+        Console.WriteLine(String.Join(", ", input))
     End Sub
 End Module
