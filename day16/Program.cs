7713dc35d45fff2ed89c3a7819117052ef11c046
fwcd <fwcdmail@gmail.com>
2020-12-16 13:51:00 +0100

    Fully implement day 16 part 1 in C#

--- day16/Program.cs
+++ day16/Program.cs
@@ -10,12 +10,12 @@ namespace day16
         private readonly List<Predicate<int>> constraints;
         private readonly List<int> fields;
 
-        public bool IsValid
+        public int ErrorRate
         {
             get
             {
                 var constraints = this.constraints;
-                return fields.All(f => constraints.All(c => c(f)));
+                return fields.Where(f => !constraints.Any(c => c(f))).Sum();
             }
         }
 
@@ -26,10 +26,29 @@ namespace day16
         }
     }
 
-    class Program
+    static class Program
     {
-        private static List<Predicate<int>> parseConstraints(IEnumerable<String> lines)
+        private static List<T> TakeWhile<T>(this IEnumerator<T> enumerator, Predicate<T> p)
         {
+            var elems = new List<T>();
+            while (enumerator.MoveNext() && p(enumerator.Current))
+            {
+                // Console.WriteLine($"Taking {enumerator.Current}");
+                elems.Add(enumerator.Current);
+            }
+            return elems;
+        }
+
+        private static void Skip<T>(this IEnumerator<T> enumerator, int n) {
+            for (int i = 0; i < n; i++)
+            {
+                enumerator.MoveNext();
+            }
+        }
+
+        private static List<Predicate<int>> parseConstraints(ref IEnumerator<String> lines)
+        {
+            Console.WriteLine("Parsing constraints");
             return lines
                 .TakeWhile(l => !String.IsNullOrWhiteSpace(l))
                 .Select(l => l.Split(":").Select(s => s.Trim()).ToList()[1])
@@ -40,8 +59,9 @@ namespace day16
                 .ToList();
         }
 
-        private static List<Ticket> parseTickets(IEnumerable<String> lines, List<Predicate<int>> constraints)
+        private static List<Ticket> parseTickets(ref IEnumerator<String> lines, List<Predicate<int>> constraints)
         {
+            Console.WriteLine("Parsing tickets");
             return lines
                 .TakeWhile(l => !String.IsNullOrWhiteSpace(l))
                 .Select(l => new Ticket(constraints, l.Split(",").Select(int.Parse).ToList()))
@@ -50,14 +70,15 @@ namespace day16
 
         static void Main(string[] args)
         {
-            var input = File.ReadAllLines("resources/input.txt");
-            var constraints = parseConstraints(input);
+            IEnumerator<string> input = File.ReadAllLines("resources/input.txt").ToList().GetEnumerator();
+            var constraints = parseConstraints(ref input);
             input.Skip(1); // 'your ticket:'
-            var myTicket = parseTickets(input, constraints).First();
+            var myTicket = parseTickets(ref input, constraints);
             input.Skip(1); // 'nearby tickets:'
-            var nearbyTickets = parseTickets(input, constraints);
+            var nearbyTickets = parseTickets(ref input, constraints);
 
-            Console.WriteLine(string.Join(", ", input));
+            var part1 = nearbyTickets.Select(t => t.ErrorRate).Sum();
+            Console.WriteLine(string.Join(", ", part1));
         }
     }
 }
