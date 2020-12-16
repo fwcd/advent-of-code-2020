7c1b5df70f6223965c79bb683d94d11ef1c9b90b
fwcd <fwcdmail@gmail.com>
2020-12-16 13:30:41 +0100

    WIP: Parse day 16 input

--- day16/Program.cs
+++ day16/Program.cs
@@ -1,13 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace day16
 {
+    struct Ticket
+    {
+        private readonly List<Predicate<int>> constraints;
+        private readonly List<int> fields;
+
+        public bool IsValid
+        {
+            get
+            {
+                var constraints = this.constraints;
+                return fields.All(f => constraints.All(c => c(f)));
+            }
+        }
+
+        public Ticket(List<Predicate<int>> constraints, List<int> fields)
+        {
+            this.constraints = constraints;
+            this.fields = fields;
+        }
+    }
+
     class Program
     {
+        private static List<Predicate<int>> parseConstraints(IEnumerable<String> lines)
+        {
+            return lines
+                .TakeWhile(l => !String.IsNullOrWhiteSpace(l))
+                .Select(l => l.Split(":").Select(s => s.Trim()).ToList()[1])
+                .SelectMany(s => s
+                    .Split("or")
+                    .Select(s => s.Trim().Split("-").Select(int.Parse).ToList())
+                    .Select(r => (Predicate<int>) (i => i >= r[0] && i <= r[1])))
+                .ToList();
+        }
+
+        private static List<Ticket> parseTickets(IEnumerable<String> lines, List<Predicate<int>> constraints)
+        {
+            return lines
+                .TakeWhile(l => !String.IsNullOrWhiteSpace(l))
+                .Select(l => new Ticket(constraints, l.Split(",").Select(int.Parse).ToList()))
+                .ToList();
+        }
+
         static void Main(string[] args)
         {
-            string[] input = File.ReadAllLines("resources/input.txt");
+            var input = File.ReadAllLines("resources/input.txt");
+            var constraints = parseConstraints(input);
+            input.Skip(1); // 'your ticket:'
+            var myTicket = parseTickets(input, constraints).First();
+            input.Skip(1); // 'nearby tickets:'
+            var nearbyTickets = parseTickets(input, constraints);
+
             Console.WriteLine(string.Join(", ", input));
         }
     }
