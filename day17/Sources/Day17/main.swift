77d974be0a46647813bff53b2fa47077e8de9566
fwcd <fwcdmail@gmail.com>
2020-12-17 07:22:58 +0100

    Implement day 17 part 2 in Swift

--- day17/Sources/Day17/main.swift
+++ day17/Sources/Day17/main.swift
@@ -1,34 +1,53 @@
 import Foundation
 
-struct Point: Hashable {
+protocol Point {
+    var withNeighbors: [Self] { get }
+}
+
+extension Point where Self: Equatable {
+    var neighbors: [Self] { withNeighbors.filter { $0 != self } }
+}
+
+struct Point3: Point, Hashable {
     let x: Int
     let y: Int
     let z: Int
 
-    var withNeighbors: [Point] {
+    var withNeighbors: [Point3] {
         (-1...1).flatMap { dz in
             (-1...1).flatMap { dy in
                 (-1...1).map { dx in
-                    Point(x: x + dx, y: y + dy, z: z + dz)
+                    Point3(x: x + dx, y: y + dy, z: z + dz)
                 }
             }
         }
     }
-    var neighbors: [Point] {
-        withNeighbors.filter { $0 != self }
+}
+
+struct Point4: Point, Hashable {
+    let x: Int
+    let y: Int
+    let z: Int
+    let w: Int
+
+    var withNeighbors: [Point4] {
+        (-1...1).flatMap { dw in
+            (-1...1).flatMap { dz in
+                (-1...1).flatMap { dy in
+                    (-1...1).map { dx in
+                        Point4(x: x + dx, y: y + dy, z: z + dz, w: w + dw)
+                    }
+                }
+            }
+        }
     }
 }
 
-struct ConwayGrid {
-    private var active: Set<Point> = []
+struct ConwayGrid<P> where P: Point & Hashable {
+    private var active: Set<P> = []
     public var activeCount: Int { active.count }
 
-    public subscript(_ z: Int, _ y: Int, _ x: Int) -> Bool {
-        get { self[Point(x: x, y: y, z: z)] }
-        set { self[Point(x: x, y: y, z: z)] = true }
-    }
-
-    public subscript(_ point: Point) -> Bool {
+    public subscript(_ point: P) -> Bool {
         get { active.contains(point) }
         set {
             if newValue {
@@ -39,8 +58,8 @@ struct ConwayGrid {
         }
     }
 
-    public func step() -> ConwayGrid {
-        var next = ConwayGrid()
+    public func step() -> Self {
+        var next = Self()
         for point in Set(active.flatMap(\.withNeighbors)) {
             let isActive = self[point]
             let activeNbCount = point.neighbors.filter { self[$0] }.count
@@ -57,22 +76,26 @@ struct ConwayGrid {
 
 func main() throws {
     let input = try String(contentsOf: URL(fileURLWithPath: "Resources/input.txt"))
-    var grid = ConwayGrid()
+    var grid1 = ConwayGrid<Point3>()
+    var grid2 = ConwayGrid<Point4>()
 
     for (y, line) in input.split(separator: "\n").map(String.init).enumerated() {
         for (x, cell) in line.enumerated() {
             if cell == "#" {
-                grid[0, y, x] = true
+                grid1[Point3(x: x, y: y, z: 0)] = true
+                grid2[Point4(x: x, y: y, z: 0, w: 0)] = true
             }
         }
     }
 
     for i in 0..<6 {
-        grid = grid.step()
-        print("at \(i): \(grid.activeCount)")
+        grid1 = grid1.step()
+        grid2 = grid2.step()
+        print("at \(i): \(grid1.activeCount) - \(grid2.activeCount)")
     }
 
-    print("Part 1: \(grid.activeCount)")
+    print("Part 1: \(grid1.activeCount)")
+    print("Part 2: \(grid2.activeCount)")
 }
 
 try main()
