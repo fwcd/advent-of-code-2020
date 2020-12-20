de816f27514987067f64f3a75c9d42b532551d8f
fwcd <fwcdmail@gmail.com>
2020-12-20 22:44:16 +0100

    Use union-find to more efficiently join edges

--- day20/src/day20.cpp
+++ day20/src/day20.cpp
@@ -1,9 +1,10 @@
 #include <fstream>
 #include <iostream>
-#include <optional>
 #include <regex>
 #include <sstream>
 #include <string>
+#include <optional>
+#include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
@@ -14,127 +15,62 @@ enum Side {
     Left
 };
 
-template<typename T>
-class Vec2 {
-public:
-    T x;
-    T y;
-    
-    Vec2(T x, T y) : x(x), y(y) {}
-    
-    Vec2<T> operator+(Vec2<T> rhs) const {
-        return Vec2(x + rhs.x, y + rhs.y);
-    }
-    
-    Vec2<T> operator*(T scale) const {
-        return Vec2(x * scale, y * scale);
-    }
-    
-    Vec2<T> rotate(int quarters) const {
-        Vec2<T> result{*this};
-        for (int i = 0; i < (quarters % 4); i++) {
-            int tmp{result.x};
-            result.x = -result.y;
-            result.y = tmp;
-        }
-        return result;
-    }
+Side oppositeSide(Side s) {
+    return static_cast<Side>((s + 2) % 4);
+}
 
-    Vec2<T> abs() const {
-        return Vec2(std::abs(x), std::abs(y));
+std::string sideToString(Side s) {
+    switch (s) {
+    case Top:
+        return "Top";
+    case Right:
+        return "Right";
+    case Bottom:
+        return "Bottom";
+    case Left:
+        return "Left";
     }
-};
+}
 
 class Tile {
 private:
-    std::vector<std::vector<int>> ids;
     std::vector<std::string> lines;
 public:
-    Tile(const std::vector<std::vector<int>>& ids, const std::vector<std::string>& lines) : ids(ids), lines(lines) {}
-    
-    char operator[](Vec2<int> pos) const {
-        return lines[pos.y][pos.x];
-    }
+    int id;
+    int parent;
+    std::unordered_map<Side, std::optional<int>> neighbors;
 
-    bool operator==(Tile other) const {
-        return ids == other.ids && lines == other.lines;
-    }
+    Tile(int parent, int id, const std::vector<std::string>& lines) :
+        id(id),
+        lines(lines),
+        parent(parent),
+        neighbors({{Top, std::nullopt}, {Left, std::nullopt}, {Bottom, std::nullopt}, {Right, std::nullopt}}) {}
 
-    std::size_t hashValue() const {
-        std::size_t result{0};
-        for (const std::vector<int>& idsLine : ids) {
-            for (int id : idsLine) {
-                result ^= std::hash<int>()(id);
+    std::string getEdge(Side side) {
+        std::string result;
+
+        switch (side) {
+        case Top:
+            result = lines.front();
+            break;
+        case Bottom:
+            result = lines.back();
+            break;
+        case Left:
+            for (int y = 0; y < lines.size(); y++) {
+                result.push_back(lines[y].front());
             }
+            break;
+        case Right:
+            for (int y = 0; y < lines.size(); y++) {
+                result.push_back(lines[y].back());
+            }
+            break;
         }
+
         return result;
     }
-    
-    Tile rotate(int quarters) const {
-        Vec2<int> offset{0, 0};
-        Vec2<int> idsOffset{0, 0};
-        Vec2<int> dx{1, 0};
-        Vec2<int> dy{0, 1};
-        Vec2<int> newSize{static_cast<int>(lines[0].size()), static_cast<int>(lines.size())};
-        Vec2<int> newIdSize{static_cast<int>(ids[0].size()), static_cast<int>(ids.size())};
 
-        dx = dx.rotate(quarters);
-        dy = dy.rotate(quarters);
-        newSize = newSize.rotate(quarters).abs();
-        newIdSize = newIdSize.rotate(quarters).abs();
-
-        switch (quarters % 4) {
-        case 0:
-            offset = Vec2(0, 0);
-            idsOffset = Vec2(0, 0);
-            break;
-        case 1:
-            offset = Vec2(newSize.x - 1, 0);
-            idsOffset = Vec2(newIdSize.x - 1, 0);
-            break;
-        case 2:
-            offset = Vec2(newSize.x - 1, newSize.y - 1);
-            idsOffset = Vec2(newIdSize.x - 1, newIdSize.y - 1);
-            break;
-        case 3:
-            offset = Vec2(0, newSize.y - 1);
-            idsOffset = Vec2(0, newIdSize.y - 1);
-            break;
-        }
-
-        std::vector<std::string> newLines{static_cast<std::size_t>(newSize.y), std::string(static_cast<std::size_t>(newSize.x), ' ')};
-        std::vector<std::vector<int>> newIds{static_cast<std::size_t>(newIdSize.y), std::vector(static_cast<std::size_t>(newIdSize.x), 0)};
-
-        for (int y = 0; y < lines.size(); y++) {
-            for (int x = 0; x < lines[y].size(); x++) {
-                Vec2 target{offset + (dy * y) + (dx * x)};
-                newLines[target.y][target.x] = lines[y][x];
-            }
-        }
-
-        for (int y = 0; y < ids.size(); y++) {
-            for (int x = 0; x < ids[y].size(); x++) {
-                Vec2 target{idsOffset + (dy * y) + (dx * x)};
-                newIds[target.y][target.x] = ids[y][x];
-            }
-        }
-        
-        return Tile(newIds, newLines);
-    }
-
-    /** Tries to glue the top of this tile with the bottom of the other. */
-    std::optional<Tile> glue(Tile other) const {
-        if (lines[0] == other.lines[other.lines.size() - 1]) {
-            std::vector<std::string> combined;
-            std::vector<std::vector<int>> combinedIds;
-            std::merge(lines.begin(), lines.end(), other.lines.begin(), other.lines.end(), std::back_inserter(combined));
-            std::merge(ids.begin(), ids.end(), other.ids.begin(), other.ids.end(), std::back_inserter(combinedIds));
-            return Tile(combinedIds, combined);
-        } else {
-            return std::nullopt;
-        }
-    }
-    
     const std::string toString() const {
         std::string result;
 
@@ -143,85 +79,92 @@ public:
             result.push_back('\n');
         }
 
-        for (const std::vector<int>& idLine : ids) {
-            std::string line;
-            for (int id : idLine) {
-                line += std::to_string(id);
-                line.push_back(' ');
-            }
-            result += line;
-            result.push_back('\n');
-        }
-
         return result;
     }
 };
 
-namespace std {
-    template<>
-    struct hash<Tile> {
-        std::size_t operator()(const Tile& tile) const {
-            return tile.hashValue();
-        }
-    };
-}
+// The jigsaw solver uses a union-find structure for efficiency
 
 class Jigsaw {
 private:
-    std::unordered_set<Tile> tiles;
+    std::unordered_set<int> roots;
 public:
+    std::vector<Tile> tiles;
+
     void addTile(const std::vector<std::string>& lines) {
         int i{static_cast<int>(tiles.size())};
         std::vector<std::string> linesMut{lines};
-        std::regex headerPattern{"Tile: (\\d+)"};
+        std::regex headerPattern{"Tile (\\d+):"};
         std::string header{linesMut.front()};
+        std::smatch matches;
+
+        std::regex_search(header, matches, headerPattern);
+        int id{std::stoi(matches[1])};
+
         linesMut.erase(linesMut.begin());
         while (linesMut.back().empty()) {
             linesMut.pop_back();
         }
-        Tile tile{{{i}}, linesMut};
-        tiles.insert(tile);
+
+        Tile tile{i, id, linesMut};
+        tiles.push_back(tile);
+        roots.insert(i);
     }
 
-    Tile solve() const {
-        std::unordered_set<Tile> remaining{tiles};
+    void solve() {
+        while (roots.size() > 1) {
+            int count = tiles.size();
+            for (int i = 0; i < count; i++) {
+                for (int j = 0; j < count; j++) {
+                    if (findRoot(i) != findRoot(j)) {
+                        for (int sa = Top; sa <= Left; sa++) {
+                            for (int sb = Top; sb <= Left; sb++) {
+                                Side sideA{static_cast<Side>(sa)};
+                                Side sideB{static_cast<Side>(sb)};
 
-        while (remaining.size() > 1) {
-            std::cout << remaining.size() << " tile(s) left" << std::endl;
-
-            for (Tile a : remaining) {
-                for (Tile b : remaining) {
-                    for (int sideA = Top; sideA <= Left; sideA++) {
-                        for (int sideB = Top; sideB <= Left; sideB++) {
-                            std::optional<Tile> glued{a.rotate(static_cast<Side>(sideA)).glue(b.rotate(static_cast<Side>(sideB)))};
-                            if (glued.has_value()) {
-                                std::cout << "GLUED:" << std::endl;
-                                std::cout << glued->toString() << std::endl;
-                                remaining.erase(a);
-                                remaining.erase(b);
-                                remaining.insert(*glued);
-                                goto outer;
+                                if (tiles[i].getEdge(sideA) == tiles[j].getEdge(sideB)) {
+                                    connect(i, j, sideA, sideB);
+                                    std::cout << roots.size() << " disjoint pieces (joined " << tiles[i].id << " " << sideToString(sideA) << " with "
+                                                                                             << tiles[j].id << " " << sideToString(sideB) << ")!" << std::endl;
+                                }
                             }
                         }
                     }
                 }
             }
-            outer: {}
-            
-            for (const Tile& tile : tiles) {
-                std::cout << tile.toString() << std::endl;
-            }
+        }
+    }
+
+    int findRoot(int i) const {
+        const Tile& tile{tiles[i]};
+        if (tile.parent != i) {
+            return findRoot(tile.parent);
+        } else {
+            return i;
+        }
+    }
+
+    void connect(int i, int j, Side sideA, Side sideB) {
+        int root1{findRoot(i)};
+        int root2{findRoot(j)};
+
+        roots.erase(root2);
+        roots.insert(root1);
+
+        tiles[i].neighbors[sideA] = j;
+        tiles[j].neighbors[sideB] = i;
+        tiles[i].parent = root1;
+        tiles[j].parent = root1;
+    }
+    
+    std::unordered_set<int> findRoots() const {
+        std::unordered_set<int> roots;
+
+        for (int i = 0; i < tiles.size(); i++) {
+            roots.insert(findRoot(i));
         }
 
-        return *remaining.begin();
-    }
-    
-    int size() const {
-        return tiles.size();
-    }
-    
-    const std::unordered_set<Tile>& getTiles() const {
-        return tiles;
+        return roots;
     }
 };
 
@@ -257,10 +200,9 @@ int main() {
     std::string input{ss.str()};
     Jigsaw jigsaw{parseJigsaw(input)};
 
-    std::cout << jigsaw.getTiles().size() << " tiles parsed!" << std::endl;
+    std::cout << jigsaw.tiles.size() << " tiles parsed!" << std::endl;
 
-    Tile result{jigsaw.solve()};
-    std::cout << result.toString() << std::endl;
+    jigsaw.solve();
 
     return 0;
 }
