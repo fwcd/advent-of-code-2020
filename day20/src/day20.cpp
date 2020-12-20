22c92597ea67e11e32bf11ea60223f23c1a3c851
fwcd <fwcdmail@gmail.com>
2020-12-20 20:05:57 +0100

    Store tiles in hash set

--- day20/src/day20.cpp
+++ day20/src/day20.cpp
@@ -1,12 +1,11 @@
 #include <fstream>
 #include <iostream>
+#include <optional>
 #include <regex>
 #include <sstream>
 #include <string>
+#include <unordered_set>
 #include <vector>
-
-// The Jigsaw-puzzle-solver uses a union-find structure to efficiently
-// represent the partially assembled tiles in memory.
 
 enum Side {
     Top = 0,
@@ -43,13 +42,30 @@ public:
 
 class Tile {
 private:
-    int parent;
+    std::vector<std::vector<int>> ids;
     std::vector<std::string> lines;
 public:
-    Tile(int parent, const std::vector<std::string>& lines) : parent(parent), lines(lines) {}
+    Tile(const std::vector<std::vector<int>>& ids, const std::vector<std::string>& lines) : ids(ids), lines(lines) {}
     
     char operator[](Vec2 pos) const {
         return lines[pos.y][pos.x];
+    }
+
+    bool operator==(Tile other) const {
+        return ids == other.ids && lines == other.lines;
+    }
+
+    std::size_t hash() const {
+        std::size_t result{0};
+        for (const std::vector<int>& idsLine : ids) {
+            for (int id : idsLine) {
+                result ^= std::hash<int>()(id);
+            }
+        }
+        for (const std::string& line : lines) {
+            result ^= std::hash<std::string>()(line);
+        }
+        return result;
     }
     
     Vec2 getCorner(Side side) const {
@@ -78,7 +94,18 @@ public:
             }
         }
         
-        return Tile(parent, result);
+        return Tile(ids, result);
+    }
+
+    /** Tries to glue the top of this tile with the bottom of the other. */
+    std::optional<Tile> glue(Tile other) const {
+        if (lines[0] == other.lines[other.lines.size() - 1]) {
+            std::vector<std::string> combined;
+            std::merge(lines.begin(), lines.end(), other.lines.begin(), other.lines.end(), std::back_inserter(combined));
+            return Tile(ids, combined);
+        } else {
+            return std::nullopt;
+        }
     }
     
     const std::string toString() const {
@@ -93,9 +120,18 @@ public:
     }
 };
 
+namespace std {
+    template<>
+    struct hash<Tile> {
+        std::size_t operator()(const Tile& tile) const {
+            return tile.hash();
+        }
+    };
+}
+
 class Jigsaw {
 private:
-    std::vector<Tile> tiles;
+    std::unordered_set<Tile> tiles;
 public:
     void addTile(const std::vector<std::string>& lines) {
         int i{static_cast<int>(tiles.size())};
@@ -103,15 +139,15 @@ public:
         std::regex headerPattern{"Tile: (\\d+)"};
         std::string header{linesMut[0]};
         linesMut.erase(linesMut.begin());
-        Tile tile{i, linesMut};
-        tiles.push_back(tile);
+        Tile tile{{{i}}, linesMut};
+        tiles.insert(tile);
     }
     
     int size() const {
         return tiles.size();
     }
     
-    const std::vector<Tile>& getTiles() const {
+    const std::unordered_set<Tile>& getTiles() const {
         return tiles;
     }
 };
