e34884783a17a11c322ccb9c25b8c81a66492076
fwcd <fwcdmail@gmail.com>
2020-12-21 15:14:56 +0100

    Fix part 1 solver for day 21

--- day21/src/main/kotlin/fwcd/day21/App.kt
+++ day21/src/main/kotlin/fwcd/day21/App.kt
@@ -9,44 +9,77 @@ data class Food(
     val allergens: List<String>
 )
 
-fun mightSatisfy(foods: List<Food>, assignment: Map<String, String?>): Boolean =
-    foods.all { food -> food.ingredients.any { !assignment.containsKey(it) }
-        || food.allergens.any { !assignment.containsValue(it) }
-        || food.allergens.any { allergen -> food.ingredients.any { assignment[it] == allergen } } }
+fun satisfies(food: Food, assignment: Map<String, String?>): Boolean =
+    food.allergens.all { allergen -> food.ingredients.any { assignment[it] == allergen } }
 
-fun satisfies(foods: List<Food>, assignment: Map<String, String?>): Boolean =
-    foods.all { food -> food.allergens
-        .all { allergen -> food.ingredients.any { assignment[it] == allergen } } }           
+fun <T> MutableList<T>.popLast(): T {
+    val last = last()
+    removeAt(size - 1)
+    return last
+}
 
-fun solveAllergens(ingreds: List<String>, allergens: List<String>, foods: List<Food>, assignment: MutableMap<String, String?>): Boolean {
-    if (ingreds.isEmpty()) {
-        return satisfies(foods, assignment)
+fun solveAllergens(ingreds: List<String>, allergens: MutableSet<String>, foods: MutableList<Food>, assignment: MutableMap<String, String?>): Boolean {
+    if (foods.isEmpty()) {
+        return allergens.isEmpty()
     } else {
-        val ingred = ingreds.last()
-        for (allergen in listOf(null) + allergens) {
-            assignment[ingred] = allergen
-            if (mightSatisfy(foods, assignment) && solveAllergens(ingreds.dropLast(1), allergens.filterNot { it == allergen }, foods, assignment)) {
-                return true
+        val food = foods.popLast()
+        println("Chose $food")
+
+        for (allergen in food.allergens) {
+            var found = false
+            
+            if (allergen in allergens) {
+                // Unassigned, try assigning
+                for (ingred in food.ingredients) {
+                    if (!assignment.containsKey(ingred)) {
+                        allergens.remove(allergen)
+                        assignment[ingred] = allergen
+                        println("$ingred -> $allergen ")
+                        if (solveAllergens(ingreds, allergens, foods, assignment)) {
+                            found = true
+                            break
+                        } else {
+                            println("  $ingred -> $allergen: no!")
+                        }
+                        assignment.remove(ingred)
+                        allergens.add(allergen)
+                    }
+                }
+            } else {
+                found = food.ingredients.any { assignment[it] == allergen }
+                println("Found $allergen in $food")
             }
-            assignment.remove(ingred)
+
+            if (!found) {
+                return false
+            }
         }
+        
+        if (solveAllergens(ingreds, allergens, foods, assignment)) {
+            println("$assignment is safe (remaining: $allergens, $foods)!")
+            return true
+        }
+        foods.add(food)
         return false
     }
 }
 
 fun main(args: Array<String>) {
-    val input = File("resources/input.txt").readText()
+    val input = File("resources/example.txt").readText()
     val foods = input.lines()
         .mapNotNull(pattern::matchEntire)
         .map { Food(it.groupValues[1].split(" "), it.groupValues[2].split(",").map { it.trim() }) }
+        .sortedBy { -it.allergens.size - it.ingredients.size }
+    
+    println(foods)
     
     val ingreds = foods.flatMap { it.ingredients }.toSet()
     val allergens = foods.flatMap { it.allergens }.toSet()
     var assignment = mutableMapOf<String, String?>()
     
-    if (solveAllergens(ingreds.toList(), allergens.toList(), foods, assignment)) {
-        println("$assignment")
-        val safeIngreds = assignment.filter { it.value == null }.map { it.key }
+    if (solveAllergens(ingreds.toList(), allergens.toMutableSet(), foods.toMutableList(), assignment)) {
+        val safeIngreds = ingreds.filterNot { assignment.containsKey(it) }
+        println("Safe: $safeIngreds | $assignment")
         println("Part 1: ${safeIngreds.map { ingred -> foods.map { it.ingredients.count { it == ingred } }.sum() }.sum()}")
     }
 }
