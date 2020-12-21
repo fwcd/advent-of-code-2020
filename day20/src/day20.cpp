5ae71f9ae0aa468cdbe4b6018e525b48eb6a17b3
fwcd <fwcdmail@gmail.com>
2020-12-21 01:20:47 +0100

    Reimplement day 20 using 2D array and backtracking

--- day20/src/day20.cpp
+++ day20/src/day20.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <exception>
 #include <fstream>
 #include <iostream>
@@ -67,16 +68,17 @@ class Tile {
 private:
     std::vector<std::string> lines;
 public:
-    int id;
+    unsigned long long id;
     int parent;
     bool flipped;
+    int rotation;
     std::unordered_map<Side, std::optional<int>> neighbors;
 
-    Tile(int parent, unsigned long long id, const std::vector<std::string>& lines) :
+    Tile(unsigned long long id, const std::vector<std::string>& lines) :
         id(id),
         lines(lines),
-        parent(parent),
         flipped(false),
+        rotation(0),
         neighbors({{Top, std::nullopt}, {Left, std::nullopt}, {Bottom, std::nullopt}, {Right, std::nullopt}}) {}
 
     std::string getEdge(Side side) {
@@ -127,13 +129,12 @@ public:
 // The jigsaw solver uses a union-find structure for efficiency
 
 class Jigsaw {
-private:
-    std::unordered_set<int> roots;
 public:
+    std::vector<std::vector<int>> grid;
     std::vector<Tile> tiles;
+    int sideLength;
 
     void addTile(const std::vector<std::string>& lines) {
-        int i{static_cast<int>(tiles.size())};
         std::vector<std::string> linesMut{lines};
         std::regex headerPattern{"Tile (\\d+):"};
         std::string header{linesMut.front()};
@@ -147,119 +148,61 @@ public:
             linesMut.pop_back();
         }
 
-        Tile tile{i, id, linesMut};
+        Tile tile{id, linesMut};
         tiles.push_back(tile);
-        roots.insert(i);
+    }
+
+    void setUp() {
+        sideLength = 2 * (static_cast<int>(std::sqrt(tiles.size())) + 1);
+        grid = std::vector(sideLength, std::vector(sideLength, -1));
     }
 
     void solve() {
-        while (roots.size() > 1) {
-            int count = tiles.size();
-            for (int i = 0; i < count; i++) {
-                for (int j = 0; j < count; j++) {
-                    if (findRoot(i) != findRoot(j)) {
-                        Tile& a = tiles[i];
-                        Tile& b = tiles[j];
-
-                        for (Side sideA : {Top, Left, Bottom, Right}) {
-                            for (Side sideB : {Top, Left, Bottom, Right}) {
-                                for (bool aFlipped : {false, true}) {
-                                    for (bool bFlipped : {false, true}) {
-                                        setOrientation(i, aFlipped);
-                                        setOrientation(j, bFlipped);
-
-                                        if (a.getEdge(sideA) == b.getEdge(sideB)) {
-                                            connect(i, j, sideA, sideB);
-                                            std::cout << roots.size() << " disjoint pieces (joined " << a.id << " " << sideToString(sideA) << " with "
-                                                                                                     << b.id << " " << sideToString(sideB) << ")!" << std::endl;
-                                            goto outer;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            outer: {}
-        }
-    }
-
-    void setOrientation(int i, bool flipped) {
-        std::unordered_set<int> visited;
-        setOrientation(i, flipped, visited);
-    }
-
-    void setOrientation(int i, bool flipped, std::unordered_set<int>& visited) {
-        if (!visited.contains(i)) {
-            visited.insert(i);
-            Tile& tile = tiles[i];
-            bool shouldFlip = flipped != tile.flipped;
-
-            if (shouldFlip) {
-                tile.flip();
-
-                for (Side side : {Top, Left, Bottom, Right}) {
-                    std::optional<int> neighbor = tile.neighbors[side];
-                    if (neighbor.has_value()) {
-                        setOrientation(*neighbor, !tiles[*neighbor].flipped, visited);
-                    }
-                }
-            }
-        }
-    }
-
-    int findRoot(int i) const {
-        const Tile& tile{tiles[i]};
-        if (tile.parent != i) {
-            return findRoot(tile.parent);
-        } else {
-            return i;
-        }
-    }
-
-    void connect(int i, int j, Side sideA, Side sideB) {
-        int root1{findRoot(i)};
-        int root2{findRoot(j)};
-
-        roots.erase(root2);
-        roots.insert(root1);
-
-        tiles[i].neighbors[sideA] = j;
-        tiles[j].neighbors[sideB] = i;
-        tiles[root2].parent = root1;
-    }
-    
-    std::unordered_set<int> findRoots() const {
-        std::unordered_set<int> roots;
-
+        std::vector<int> remaining;
         for (int i = 0; i < tiles.size(); i++) {
-            roots.insert(findRoot(i));
+            remaining.push_back(i);
         }
-
-        return roots;
+        solve(remaining);
     }
 
-    std::unordered_map<int, Vec2> toGrid() {
-        if (roots.size() > 1) {
-            throw std::runtime_error("Please solve the puzzle before converting to a grid!");
+    bool linesUp(int i, int nx, int ny, Side s1, Side s2) {
+        if (ny < 0 || ny >= sideLength || nx < 0 || nx >= sideLength) {
+            return true;
         }
-        std::unordered_map<int, Vec2> grid;
-        dfsToGrid(0, Vec2(0, 0), grid);
-        return grid;
+        int j{grid[ny][nx]};
+        if (j < 0) {
+            return true;
+        }
+        return tiles[i].getEdge(s1) == tiles[j].getEdge(s2);
     }
 
-    void dfsToGrid(int i, Vec2 pos, std::unordered_map<int, Vec2>& grid) {
-        if (!grid.contains(i)) {
-            grid[i] = pos;
-            Tile& tile = tiles[i];
+    bool canPlace(int i, int x, int y) {
+        return linesUp(i, x - 1, y, Left, Right)
+            && linesUp(i, x + 1, y, Right, Left)
+            && linesUp(i, x, y - 1, Top, Bottom)
+            && linesUp(i, x, y + 1, Bottom, Top);
+    }
 
-            for (Side side : {Top, Left, Bottom, Right}) {
-                std::optional<int> neighbor = tile.neighbors[side];
-                if (neighbor.has_value()) {
-                    dfsToGrid(*neighbor, pos + sideToDir(side), grid);
+    bool solve(std::vector<int>& remaining) {
+        std::cout << remaining.size() << " remaining" << std::endl;
+        if (remaining.empty()) {
+            return true;
+        } else {
+            int i = remaining.back();
+            remaining.pop_back();
+            for (int y = 0; y < sideLength; y++) {
+                for (int x = 0; x < sideLength; x++) {
+                    if (canPlace(i, x, y)) {
+                        grid[y][x] = i;
+                        if (solve(remaining)) {
+                            return true;
+                        }
+                        grid[y][x] = -1;
+                    }
                 }
             }
+            remaining.push_back(i);
+            return false;
         }
     }
 };
@@ -285,6 +228,7 @@ Jigsaw parseJigsaw(const std::string& raw) {
         jigsaw.addTile(split(rawTile, "\n"));
     }
 
+    jigsaw.setUp();
     return jigsaw;
 }
 
@@ -299,37 +243,36 @@ int main() {
     std::cout << jigsaw.tiles.size() << " tiles parsed!" << std::endl;
 
     jigsaw.solve();
-    std::unordered_map<int, Vec2> grid{jigsaw.toGrid()};
     
     // Find the corners
 
-    Vec2 tl, tr, bl, br;
-    int tlTile, trTile, blTile, brTile;
+    // Vec2 tl, tr, bl, br;
+    // int tlTile, trTile, blTile, brTile;
 
-    for (const auto& [i, pos] : grid) {
-        if (pos.x < tl.x || pos.y < tl.y) {
-            tl = pos;
-            tlTile = i;
-        }
-        if (pos.x > tr.x || pos.y < tr.y) {
-            tr = pos;
-            trTile = i;
-        }
-        if (pos.x < bl.x || pos.y > bl.y) {
-            bl = pos;
-            blTile = i;
-        }
-        if (pos.x > br.x || pos.y > br.y) {
-            br = pos;
-            brTile = i;
-        }
+    // for (const auto& [i, pos] : grid) {
+    //     if (pos.x < tl.x || pos.y < tl.y) {
+    //         tl = pos;
+    //         tlTile = i;
+    //     }
+    //     if (pos.x > tr.x || pos.y < tr.y) {
+    //         tr = pos;
+    //         trTile = i;
+    //     }
+    //     if (pos.x < bl.x || pos.y > bl.y) {
+    //         bl = pos;
+    //         blTile = i;
+    //     }
+    //     if (pos.x > br.x || pos.y > br.y) {
+    //         br = pos;
+    //         brTile = i;
+    //     }
 
-        std::cout << "at " << pos.x << ", " << pos.y << ": " << jigsaw.tiles[i].id << std::endl;
-    }
+    //     std::cout << "at " << pos.x << ", " << pos.y << ": " << jigsaw.tiles[i].id << std::endl;
+    // }
 
-    unsigned long long tlId = jigsaw.tiles[tlTile].id, trId = jigsaw.tiles[trTile].id, blId = jigsaw.tiles[blTile].id, brId = jigsaw.tiles[brTile].id;
-    std::cout << "Corners: " << tlId << ", " << trId << ", " << blId << ", " << brId << std::endl;
-    std::cout << "Part 1: " << (tlId * trId * blId * brId) << std::endl;
+    // unsigned long long tlId = jigsaw.tiles[tlTile].id, trId = jigsaw.tiles[trTile].id, blId = jigsaw.tiles[blTile].id, brId = jigsaw.tiles[brTile].id;
+    // std::cout << "Corners: " << tlId << ", " << trId << ", " << blId << ", " << brId << std::endl;
+    // std::cout << "Part 1: " << (tlId * trId * blId * brId) << std::endl;
 
     return 0;
 }
