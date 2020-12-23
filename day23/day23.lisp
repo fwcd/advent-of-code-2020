ddb269c043cced3909860f41e391d7341d03f8e6
fwcd <fwcdmail@gmail.com>
2020-12-23 18:22:16 +0100

    Define cyclic lists in day 23 (Lisp)

--- day23/day23.lisp
+++ day23/day23.lisp
@@ -1,9 +1,10 @@
-(defun read-file (name)
-  (with-open-file (stream name)
-    (loop for line = (read-line stream nil)
-          while line
-          collect line)))
+(defun circle (xs)
+  (let ((ys (copy-tree xs)))
+    (setf (cdr (last ys)) ys)))
+
+(defun move (n cups))
 
 (defun main ()
-  (let ((input (read-file "resources/input.txt")))
-    (format t "~S~%" input)))
+  (setq *print-circle* t)
+  (let ((part1 (circle (list 9 1 6 4 3 8 2 7 5))))
+    (format t "Part 1: ~S~%" part1)))
