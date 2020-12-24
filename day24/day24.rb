5367f8c499df6f1c82b5a608593cb66036fedbde
fwcd <fwcdmail@gmail.com>
2020-12-24 10:43:17 +0100

    Implement day 24 part 1 fully

--- day24/day24.rb
+++ day24/day24.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 # We use cube coords to identify positions on the hex grid.
 # See https://www.redblobgames.com/grids/hexagons/#coordinates
 Coords = Struct.new(:x, :y, :z) do
@@ -7,29 +9,38 @@ Coords = Struct.new(:x, :y, :z) do
 end
 
 $dirs = {
-  "nw" => Coords.new(0, 1, -1),
-  "ne" => Coords.new(1, 0, -1),
-  "e"  => Coords.new(1, -1, 0),
-  "se" => Coords.new(0, -1, 1),
-  "sw" => Coords.new(-1, 0, 1),
-  "w"  => Coords.new(-1, 1, 0)
+  'nw' => Coords.new(0, 1, -1),
+  'ne' => Coords.new(1, 0, -1),
+  'e'  => Coords.new(1, -1, 0),
+  'se' => Coords.new(0, -1, 1),
+  'sw' => Coords.new(-1, 0, 1),
+  'w'  => Coords.new(-1, 1, 0)
 }
 
 def parse_path(s)
-  unless s.empty?
+  unless s.strip.empty?
     raw = ''
     while not $dirs.key?(raw)
       raw += s[0]
       s = s[1..]
     end
-    rest = parse_path(s)
-    puts rest
-    dir = $dirs[raw]
-    puts dir
-    return (dir + rest)
+    return $dirs[raw] + parse_path(s)
   else
     return Coords.new(0, 0, 0)
   end
 end
 
-parse_path "sesenwnenenewseeswwswswwnenewsewsw"
+black_tiles = Set[]
+
+File.open('resources/input.txt', 'r') do |f|
+  f.each_line do |line|
+    tile = parse_path(line)
+    if black_tiles.include?(tile)
+      black_tiles.delete(tile)
+    else
+      black_tiles.add(tile)
+    end
+  end
+end
+
+puts "Part 1: #{black_tiles.size()}"
