a488c16eb3801a21e04b2eeffe3508ca05037569
fwcd <fwcdmail@gmail.com>
2020-12-24 01:25:52 +0100

    Use precomputed min/max for day 23, part 2
    The solution now computes in a few seconds

--- day23/day23.lisp
+++ day23/day23.lisp
@@ -50,7 +50,7 @@
   (circle-find-impl f xs xs))
 
 (defun iterate (n f x)
-  (format t "~S~%" n)
+  ;; (format t "~S~%" n)
   (if (> n 0) (iterate (- n 1) f (funcall f x))
               x))
 
@@ -64,14 +64,13 @@
                   (if zs zs
                          (dest-cup (- x 1) mn mx ht)))))
 
-(defun move (xs ht)
+(defun move (xs ht mn mx)
   (let ((x (car xs))
         (ts (take 3 (cdr xs))))
     (loop for x in (take-nodes 3 (cdr xs)) do
       (remhash (car x) ht))
     (setf (cdr xs) (drop 3 (cdr xs)))
-    (let* ((xsl (cdr (circle-unroll xs)))
-           (zs (dest-cup (- x 1) (apply 'min xsl) (apply 'max xsl) ht)))
+    (let ((zs (dest-cup (- x 1) mn mx ht)))
       (setf (cdr zs) (append ts (cdr zs)))
       (loop for x in (take-nodes 3 (cdr zs)) do
         (setf (gethash (car x) ht) x))
@@ -81,22 +80,25 @@
   (setf (gethash (car xs) ht) xs)
   (if (eq xs start) nil (store-nodes-in-hash-table (cdr xs) ht start)))
 
-(defun play (n xs)
+(defun play (n xs gmn gmx)
   (let ((ht (make-hash-table)))
     (store-nodes-in-hash-table (cdr xs) ht xs)
-    (iterate n (lambda (ys) (move ys ht)) xs)))
+    (format t "Playing...~%")
+    (iterate n (lambda (ys) (let* ((y (car ys))
+                                   (mn (if (eql y gmn) (+ 1 gmn) gmn))
+                                   (mx (if (eql y gmx) (- 1 gmx) gmx))) (move ys ht mn mx))) xs)))
 
 (defun main ()
   (declare (optimize (speed 3) (debug 0) (safety 0)))
   (setq *print-circle* t)
   (let* ((inputl1 '(9 1 6 4 3 8 2 7 5))
          (input1 (circle inputl1))
-         (final1 (cdr (circle-find (lambda (x) (eql x 1)) (play 100 input1)))))
+         (final1 (cdr (circle-find (lambda (x) (eql x 1)) (play 100 input1 1 9)))))
     (format t "Part 1: ~S~%" final1)
     (let* ((n (length inputl1))
            (mx (apply 'max inputl1))
            (inputl2 (append inputl1 (range (+ mx 1) (+ (+ mx 1) (- 1000000 n)))))
            (input2 (circle inputl2))
-           (final2 (cdr (circle-find (lambda (x) (eql x 1)) (play 10000000 input2))))
+           (final2 (cdr (circle-find (lambda (x) (eql x 1)) (play 10000000 input2 1 1000000))))
            (part2 (* (car final2) (car (cdr final2)))))
       (format t "Part 2: ~S~%" part2))))
